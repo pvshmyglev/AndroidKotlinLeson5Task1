e55f31db6858f076ab2fd56ac4b243827043fd25
pvshmyglev <pshm2@mail.ru>
2022-05-08 23:37:05 +0300

    mvvm

--- app/src/main/java/ru/netology/nmedia/Post.kt
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -1,12 +1,17 @@
 package ru.netology.nmedia
 
 data class Post(
+
     val id: Int,
     val author: String,
     val content: String,
     val publishedDate: String,
-    var likeByMe: Boolean,
-    var countLikes: Int,
-    var countShare: Int,
-    var countVisibility: Int
+    val likeByMe: Boolean,
+    val countLikes: Int,
+    val countLikesText: String,
+    val countShare: Int,
+    val countShareText: String,
+    val countVisibility: Int,
+    val countVisibilityText: String
+
     )
